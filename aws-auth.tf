cecb6ea463e278e5091e0fe2cb3362d032475953
Nathaneil2025 <seanbad85@gmail.com>
2025-09-05 18:06:20 +0300

    another change7

--- aws-auth.tf
+++ aws-auth.tf
@@ -1,102 +1,30 @@
-resource "aws_eks_cluster" "cluster" {
-  name     = "${var.project_name}-eks"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = var.eks_version
+# aws-auth.tf
 
-  vpc_config {
-    subnet_ids              = [aws_subnet.public.id, aws_subnet.private.id]
-    endpoint_public_access  = true
-    endpoint_private_access = false
+# This only manages RBAC access (maps IAM roles/users to Kubernetes RBAC)
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = jsonencode([
+      {
+        rolearn  = aws_iam_role.eks_node_role.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+    mapUsers = jsonencode([
+      {
+        userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Nateuser"
+        username = "nate"
+        groups   = ["system:masters"]
+      }
+    ])
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_cluster_VPCResourceController
+    aws_eks_cluster.cluster
   ]
-
-  tags = {
-    Name    = "${var.project_name}-eks"
-    Project = var.project_name
-  }
-}
-
-# Node group in PUBLIC subnet
-resource "aws_eks_node_group" "public_ng" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "${var.project_name}-public-ng"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.public.id]
-  instance_types  = var.instance_types
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 2
-  }
-
-  remote_access {
-    ec2_ssh_key               = var.key_name
-    source_security_group_ids = [aws_security_group.nodes.id]
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  ami_type = "AL2_x86_64"
-
-  labels = {
-    role = "public"
-  }
-
-  depends_on = [
-    aws_eks_cluster.cluster,
-    kubernetes_config_map.aws_auth,  # ✅ Ensure aws-auth applied first
-    aws_security_group_rule.controlplane_to_nodes_ephemeral,
-    aws_security_group_rule.controlplane_to_nodes_https
-  ]
-
-  tags = {
-    Project = var.project_name
-  }
-}
-
-# Node group in PRIVATE subnet
-resource "aws_eks_node_group" "private_ng" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "${var.project_name}-private-ng"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.private.id]
-  instance_types  = var.instance_types
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 2
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  ami_type = "AL2_x86_64"
-
-  labels = {
-    role = "private"
-  }
-
-  depends_on = [
-    aws_eks_cluster.cluster,
-    kubernetes_config_map.aws_auth,  # ✅ Ensure aws-auth applied first
-    aws_security_group_rule.controlplane_to_nodes_ephemeral,
-    aws_security_group_rule.controlplane_to_nodes_https
-  ]
-
-  tags = {
-    Project = var.project_name
-  }
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.cluster.name
 }
