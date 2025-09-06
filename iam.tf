707959ad09407d6f446e014a4cdd6c015743f916
Nathaneil2025 <seanbad85@gmail.com>
2025-09-06 19:32:35 +0300

    change 22

--- iam.tf
+++ iam.tf
@@ -1,30 +1,66 @@
-data "aws_caller_identity" "current" {}
+# EKS Cluster Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.project_name}-eks-cluster-role"
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "GitHubActionsRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-      },
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-          # Restrict to your repo
-          "token.actions.githubusercontent.com:sub" = "repo:Nathaneil2025/Lawyal:ref:refs/heads/main"
-        }
-      }
-    }]
-  })
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 
-resource "aws_iam_role_policy" "github_actions_destroy" {
-  name = "GitHubActionsDestroyPolicy"
-  role = aws_iam_role.github_actions_role.id
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_VPCResourceController" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# Node Group Role
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-eks-node-role"
+
+  assume_role_policy = data.aws_iam_policy_document.nodes_assume_role.json
+}
+
+data "aws_iam_policy_document" "nodes_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+
+resource "aws_iam_user_policy" "nateuser_ec2_cleanup" {
+  name = "Nateuser-EC2-Cleanup"
+  user = "Nateuser"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -32,48 +68,26 @@ resource "aws_iam_role_policy" "github_actions_destroy" {
       {
         Effect = "Allow",
         Action = [
-          # EKS
-          "eks:DeleteCluster",
-          "eks:DescribeCluster",
-          "eks:ListNodegroups",
-          "eks:DeleteNodegroup",
-          "eks:DescribeNodegroup",
-          "eks:UpdateNodegroupConfig",
-
-          # Auto Scaling (for backing ASGs of nodegroups)
-          "autoscaling:DeleteAutoScalingGroup",
-          "autoscaling:DescribeAutoScalingGroups",
-
-          # EC2
-          "ec2:TerminateInstances",
-          "ec2:DescribeInstances",
-          "ec2:DeleteNetworkInterface",
+          # Network Interface cleanup
+          "ec2:DescribeNetworkInterfaces",
           "ec2:DetachNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+
+          # Security groups cleanup
+          "ec2:DescribeSecurityGroups",
           "ec2:DeleteSecurityGroup",
+
+          # VPC/Subnet/IGW cleanup (if needed)
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
           "ec2:DeleteSubnet",
-          "ec2:DeleteVpc",
-          "ec2:DeleteNatGateway",
-          "ec2:ReleaseAddress",
-          "ec2:DeleteRouteTable",
+          "ec2:DescribeInternetGateways",
           "ec2:DetachInternetGateway",
           "ec2:DeleteInternetGateway",
-          "ec2:Describe*",
 
-          # ELB
-          "elasticloadbalancing:DeleteLoadBalancer",
-          "elasticloadbalancing:DescribeLoadBalancers",
-          "elasticloadbalancing:DeleteTargetGroup",
-          "elasticloadbalancing:DescribeTargetGroups",
-
-          # ECR
-          "ecr:BatchDeleteImage",
-          "ecr:DeleteRepository",
-          "ecr:ListImages",
-          "ecr:DescribeRepositories",
-
-          # DynamoDB (for TF state locks)
-          "dynamodb:DeleteItem",
-          "dynamodb:Scan"
+          # Instances (if not already covered)
+          "ec2:DescribeInstances",
+          "ec2:TerminateInstances"
         ],
         Resource = "*"
       }
