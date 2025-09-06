53fd1645278c818353113cf24ec803435f02ff7b
Nathaneil2025 <seanbad85@gmail.com>
2025-09-06 19:10:39 +0300

    change 19

--- eks.tf
+++ eks.tf
@@ -1,4 +1,53 @@
-# Node group in PUBLIC subnet
+############################################
+# EKS Cluster
+############################################
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.project_name}-eks"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.eks_version
+
+  vpc_config {
+    subnet_ids              = [aws_subnet.public.id, aws_subnet.private.id]
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public,
+    aws_subnet.private,
+    aws_internet_gateway.igw,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_VPCResourceController
+  ]
+
+  tags = {
+    Name    = "${var.project_name}-eks"
+    Project = var.project_name
+  }
+}
+
+############################################
+# IAM Policies for Worker Node Role
+############################################
+resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+############################################
+# Node Group (Public Subnet)
+############################################
 resource "aws_eks_node_group" "public_ng" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.project_name}-public-ng"
@@ -29,7 +78,7 @@ resource "aws_eks_node_group" "public_ng" {
 
   depends_on = [
     aws_eks_cluster.cluster,
-    kubernetes_config_map.aws_auth, # 👈 ensure IAM mappings applied
+    kubernetes_config_map.aws_auth, # ✅ ensure IAM mapping exists
     aws_security_group_rule.controlplane_to_nodes_ephemeral,
     aws_security_group_rule.controlplane_to_nodes_https,
     aws_iam_role_policy_attachment.eks_worker_AmazonEKSWorkerNodePolicy,
@@ -42,7 +91,9 @@ resource "aws_eks_node_group" "public_ng" {
   }
 }
 
-# Node group in PRIVATE subnet
+############################################
+# Node Group (Private Subnet)
+############################################
 resource "aws_eks_node_group" "private_ng" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "${var.project_name}-private-ng"
@@ -68,7 +119,7 @@ resource "aws_eks_node_group" "private_ng" {
 
   depends_on = [
     aws_eks_cluster.cluster,
-    kubernetes_config_map.aws_auth, # 👈 ensure IAM mappings applied
+    kubernetes_config_map.aws_auth, # ✅ ensure IAM mapping exists
     aws_security_group_rule.controlplane_to_nodes_ephemeral,
     aws_security_group_rule.controlplane_to_nodes_https,
     aws_iam_role_policy_attachment.eks_worker_AmazonEKSWorkerNodePolicy,
