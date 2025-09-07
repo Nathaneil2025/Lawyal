972dda961ff51f70c6a0079c14633a2c28b44e0e
Nathaneil2025 <seanbad85@gmail.com>
2025-09-07 22:08:12 +0300

    change 9

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on AWS EKS 🚀"
+    return "Hello from Flask on AWS EKS 2 🚀"
 
 @app.route("/health")
 def health():
